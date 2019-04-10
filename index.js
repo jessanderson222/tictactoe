ea715fae758f94b048a8134dd9d56502e711cea9
jessanderson222 <jessanderson222@gmail.com>
2019-04-10 10:02:13 -0400

    Take 2

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-// const startButton = document.querySelector("button");
-// const squareTwo = document.querySelector("square-2");
-document.addEventListener("DOMContentLoaded", () => {
-  const startButton = document.querySelector("button");
-  const squareTwo = document.querySelector("square-2");
-  console.log("loaded");
-  startButton.addEventListener("click", eventHandler);
-});
+var winners = new Array(); // all possible winning combinations
+var player1Selections = new Array(); //player1 selections
+var player2Selections = new Array(); //player2 selections
+var currentPlayer = 0; //index of player currently in play
+var points1 = 0; //ongoing games player scores
+var points2 = 0;
+var size = 3; //board measure in width * height
 
-function eventHandler(e) {
-  e.preventDefault();
-  startGame();
-}
+function drawBoard() {
+  var parent = document.getElementById("game");
+  var counter = 1;
 
-function startGame() {
-  score = {
-    X: 0,
-    O: 0
-  };
-  moves = 0;
-  turn = "X";
+  for (var i = 0; i < 3; i++) {
+    var row = document.createElement("tr");
+
+    for (var x = 0; x < size; x++) {
+      var col = document.createElement("td");
+      col.innerHTML = counter;
+
+      row.appendChild(col);
+    }
+    parent.appendChild(row);
+  }
 }
